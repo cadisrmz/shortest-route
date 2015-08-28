07da08028ff3cb04ccc71ce490c37512051b6a86
Navneet Karnani <navneetkarnani@gmail.com>
2015-08-29 01:25:56 +0530

    Add tests to get coverage on all conditions

--- src/test/java/projects/nk/shortestroute/data/DataGraphTest.java
+++ src/test/java/projects/nk/shortestroute/data/DataGraphTest.java
@@ -1,9 +1,6 @@
 package projects.nk.shortestroute.data;
 
-import static org.junit.Assert.assertArrayEquals;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 import java.util.List;
 
@@ -17,57 +14,78 @@ public class DataGraphTest {
 	private static final String BOM = "BOM";
 	private static final String PNQ = "PNQ";
 	private static final String TEST_SAMPLE_DATA_TXT = "src/test/resources/test-sample-data.txt";
-	
+		
 	@Test
-	public void testPopulateGraphOneNode() throws ObjectNotFoundException {
+	public void testFindInvalidNode() throws ObjectNotFoundException {
 		DataGraph dataGraph = new DataGraph();
 		dataGraph.populateGraph(BOM, PNQ);
 		
-		assertTrue(dataGraph.hasGraphNode(BOM));
-		assertTrue(dataGraph.hasGraphNode(PNQ));
+		assertFalse("Should not find this node since it was not added", dataGraph.hasGraphNode(DEL));
+	}
+	
+	public void testFindInvalidPath() throws ObjectNotFoundException {
+		DataGraph dataGraph = new DataGraph();
+		dataGraph.populateGraph(BOM, PNQ);
+		dataGraph.populateGraph(DEL, PNQ);
+		dataGraph.populateGraph(BOM, DEL);
 		
-		// PNQ should not be connected to anything else
-		assertEquals(dataGraph.getConnectionCount(PNQ), 0);
+		assertFalse("This path was never added", dataGraph.isConnected(PNQ, DEL));
+	}
+	
+	@Test(expected=ObjectNotFoundException.class)
+	public void testFindInvalidNodeInPath() throws ObjectNotFoundException {
+		DataGraph dataGraph = new DataGraph();
+		dataGraph.populateGraph(BOM, PNQ);
+		dataGraph.populateGraph(DEL, PNQ);
+		dataGraph.populateGraph(BOM, DEL);
 		
-		// BOM should not be connected to PNQ and nothing else
-		assertEquals(dataGraph.getConnectionCount(BOM), 1);
-		assertTrue(dataGraph.isConnected(BOM,PNQ));
+		dataGraph.isConnected(PNQ, "MAA");
 	}
 	
 	@Test
-	public void testPopulateGraphTwoNode() throws ObjectNotFoundException {
+	public void testPopulateGraph() throws ObjectNotFoundException {
+		DataGraph dataGraph = new DataGraph();
+		dataGraph.populateGraph(BOM, PNQ);
+		
+		assertTrue("Should find this node",dataGraph.hasGraphNode(BOM));
+		assertTrue("Should find this node",dataGraph.hasGraphNode(PNQ));
+		
+		// PNQ should not be connected to anything else
+		assertEquals("There should be exactly 0 outgoing paths from here", 0, dataGraph.getConnectionCount(PNQ));
+		
+		// BOM should not be connected to PNQ and nothing else
+		assertEquals("There should be exactly 1 outgoing paths from here", 1, dataGraph.getConnectionCount(BOM));
+		assertTrue("Needs to have this path", dataGraph.isConnected(BOM,PNQ));
+	}
+	
+	@Test
+	public void testConnectionCount() throws ObjectNotFoundException {
 		DataGraph dataGraph = new DataGraph();
 		dataGraph.populateGraph(BOM, PNQ);
 		dataGraph.populateGraph(BOM, DEL);
 		
 		// PNQ should not be connected to anything else
-		assertTrue(dataGraph.hasGraphNode(PNQ));
-		assertEquals(dataGraph.getConnectionCount(PNQ), 0);
+		assertEquals(0, dataGraph.getConnectionCount(PNQ));
 		
 		// DEL should not be connected to anything else
-		assertNotNull(dataGraph.hasGraphNode(DEL));
-		assertEquals(dataGraph.getConnectionCount(DEL), 0l);
+		assertEquals(0, dataGraph.getConnectionCount(DEL));
 		
 		// BOM should not be connected to PNQ and DEL and nothing else
-		assertTrue(dataGraph.hasGraphNode(BOM));
-		assertEquals(dataGraph.getConnectionCount(BOM), 2);
+		assertEquals(2, dataGraph.getConnectionCount(BOM));
 		assertTrue(dataGraph.isConnected(BOM, PNQ));
-		assertTrue(dataGraph.isConnected(BOM, DEL));
+		assertTrue(dataGraph.isConnected(BOM, DEL));	
 	}
 
 	@Test
-	public void testfindPathsMSPtoAMS() {
+	public void testGraphWithCyclicPath() throws ObjectNotFoundException {
 		DataGraph dataGraph = new DataGraph();
-		
-		ShortestPath app = new ShortestPath();
-		app.readDataFile(TEST_SAMPLE_DATA_TXT, dataGraph);
+		dataGraph.populateGraph(BOM, PNQ);
+		dataGraph.populateGraph(BOM, DEL);
+		dataGraph.populateGraph(PNQ, DEL);
+		dataGraph.populateGraph(DEL, PNQ);
+		dataGraph.populateGraph(PNQ, BOM);
 
-		List<String[]> path = dataGraph.findPaths("MSP", "AMS");
-		
-		assertEquals("Validate path from MSP to AMS", path.size(), 2);
-		assertArrayEquals(path.get(0), new String[] {"MSP", "AMS"});
-		assertArrayEquals(path.get(1), new String[] {"MSP", "FFK", "AMS"});
-
+		// BOM should not be connected to PNQ and DEL and nothing else
+		assertEquals(2, dataGraph.findPaths(BOM, DEL).size());
 	}
-
 }
