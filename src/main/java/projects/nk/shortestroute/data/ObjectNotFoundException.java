ab7fb8692d6afd1bff1d78f8317aec2ce46c34a8
Navneet Karnani <navneetkarnani@gmail.com>
2015-08-31 00:08:42 +0530

    Add serialVersionUID to Exception to enable Serialization

--- src/main/java/projects/nk/shortestroute/data/ObjectNotFoundException.java
+++ src/main/java/projects/nk/shortestroute/data/ObjectNotFoundException.java
@@ -2,4 +2,9 @@ package projects.nk.shortestroute.data;
 
 public class ObjectNotFoundException extends Exception {
 
+	/**
+	 * Generated serialVersionUID to ensure Serialization works 
+	 */
+	private static final long serialVersionUID = -8120714448771885435L;
+
 }
